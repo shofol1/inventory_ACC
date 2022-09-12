ac975a0cd373e17be301117ea41b717044a025bd
shofol <18103156@iubat.edu>
2022-09-12 15:22:03 +0600

    query builder

--- controllers/controller.product.js
+++ controllers/controller.product.js
@@ -1,5 +1,9 @@
 const { Product } = require("../models/model.product");
 
+exports.welcome = (req, res) => {
+  res.send("Route is working! YaY!");
+};
+
 exports.insertProduct = async (req, res, next) => {
   try {
     const newProduct = req.body;
@@ -22,5 +26,39 @@ exports.insertProduct = async (req, res, next) => {
       message: "data not inserted",
       error: error.message,
     });
+  }
+};
+exports.getProduct = async (req, res) => {
+  try {
+    // const product = await Product.find({
+    //   $or: [{ name: "iphone" }, { quantity: 100 }],
+    // });
+    // const product = await Product.find({
+    //   status: { $ne: "out-of-stock" },
+    // });
+    // const product = await Product.find({
+    //   quantity: { $lt: 500 },
+    // });
+    // const product = await Product.find({
+    //   quantity: { $gte: 500 },
+    // });
+    // const product = await Product.find({}, "-name -price");
+    // const product = await Product.find({}, "-name -price").limit(2);
+    // const product = await Product.find({}, "-name -price").sort({
+    //   quantity: -1,
+    // });
+    // const product = await Product.find({}).select({name: 1 });
+    // const product = await Product.find({}).select({ _id: 0 });
+
+    //using query builder
+    const product = await await Product.where("name")
+      .equals("iphone")
+      .where("price")
+      .gt(100)
+      .limit(1);
+
+    res.json({ data: product });
+  } catch (error) {
+    res.json({ status: 400, message: error.message });
   }
 };
--- routes/route.product.js
+++ routes/route.product.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { insertProduct } = require("../controllers/controller.product");
+const {
+  insertProduct,
+  welcome,
+  getProduct,
+} = require("../controllers/controller.product");
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => {
-  res.send("Route is working! YaY!");
-});
+userRouter.get("/", welcome);
+userRouter.get("/all-product", getProduct);
 userRouter.post("/product", insertProduct);
 
 module.exports = { userRouter };
