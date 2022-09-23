757031bf0c300e9d0d11dd17115b8cdb02adfbe6
shofol <18103156@iubat.edu>
2022-09-23 19:51:58 +0600

    product model refactoring

--- models/model.product.js
+++ models/model.product.js
@@ -4,66 +4,48 @@ const productSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Please insert product name"],
+      required: [true, "Please provide a  product name"],
       trim: true,
       unique: true,
       minLength: [3, "Product name at least 3 characters"],
       maxLength: [100, "Product name is too large"],
+      lowercase: true,
     },
     description: {
       type: String,
       required: true,
     },
-    price: {
-      type: Number,
-      required: true,
-      min: [0, "Price can't be negative"],
-    },
     unit: {
       type: String,
       required: true,
       enum: {
-        values: ["kg", "liter", "pcs"],
+        values: ["kg", "liter", "pcs", "bag"],
         message: "Unit cant be {VALUE},must be kg/liter/pcs",
       },
     },
-    quantity: {
-      type: Number,
-      required: true,
-      min: [0, "Quantity cant be negative"],
-      validator: {
-        validate(value) {
-          const isLength = Number.isInteger(value);
-          if (isLength) {
-            return true;
-          } else {
-            return false;
-          }
+    imageUrl: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        validate: {
+          validator: (value) => {
+            let isValid = true;
+            if (!Array.isArray(value)) {
+              return false;
+            } else {
+              value.forEach((url) => {
+                if (!validator.isURL(url)) {
+                  isValid = false;
+                }
+              });
+            }
+            return isValid;
+          },
         },
       },
-      message: "Quantity must be integer",
-    },
-    status: {
-      type: String,
-      required: true,
-      enum: {
-        values: ["in-stock", "out-of-stock", "discontinued"],
-        message: "Can't be {VALUE}",
-      },
-    },
-    // supplier: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "Supplier",
-    // },
-    // categories: [
-    //   {
-    //     name: {
-    //       type: String,
-    //       required: true,
-    //     },
-    //     _id: mongoose.Schema.Types.ObjectId,
-    //   },
-    // ],
+    ],
   },
   {
     timestamps: true,
